918b28c570f15790a61bdd64a489cabefc03cbd8
Ivan Robles <irae45@gmail.com>
2024-06-12 18:36:58 -0600

    remove unnecesary CSS lines at minimal react template.

--- templates/minimal-react/src/App.jsx
+++ templates/minimal-react/src/App.jsx
@@ -11,9 +11,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
     </>
   )
