d04a66e5614aa37d955bb7401f07b5a6c09d2789
Ivan Robles <irae45@gmail.com>
2024-06-21 20:58:41 -0600

    add template react tailwind eslint testing library.

---
+++ templates/react-tw-lint-test/__tests__/App.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from '../src/App';
+
+test('App component render', async () => {
+  render(<App />);
+
+  expect(screen.getByRole('button')).toHaveTextContent('count');
+});
