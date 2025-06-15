f179994e633ff5af26fb7be8c9ef8e53709166e1
Ivan Robles <irae45@gmail.com>
2025-06-14 19:58:13 -0600

    add template categories.

--- src/templateList.js
+++ src/templateList.js
@@ -1,66 +1,59 @@
 import colors from './utils/colors';
+import setColor from './utils/setColor';
 
-const templates = [
-  {
-    value: 'vanilla-js',
-    title: colors.vanilla('Vanilla JS'),
-    description: colors.error('Vanilla JS + Vite.'),
+const templateCategories = {
+  'Frontend': {
+    'VanillaJS': [
+      {
+        value: 'vanilla-js',
+        title: setColor('VanillaJS'),
+        description: colors.error('Vanilla JS + Vite.'),
+      },
+    ],
+    'React JavaScript': [
+      {
+        value: 'react',
+        title: setColor('React Vite'),
+        description: colors.error('React + Vite.'),
+      },
+      {
+        value: 'react-dev',
+        title:  setColor('React Dev'),
+        description: colors.error('React + Vite + Tailwind CSS + ESlint + Jest + React Testing Library.'),
+      },
+      {
+        value: 'react-webpack',
+        title: setColor('React Webpack'),
+        description: colors.error('React + Webpack.')
+      },
+      {
+        value: 'react-dev-webpack',
+        title: setColor('React Dev Webpack'),
+        description: colors.error('React + Webpack + Tailwind CSS + ESlint + Jest + React Testing Library.')
+      },
+    ],
+    'React TypeScript': [
+      {
+        value: 'react-ts',
+        title:  setColor('React TypeScript'),
+        description: colors.error('React + Vite + TypeScript + ESlint.'),
+      },
+      {
+        value: 'react-dev-ts',
+        title:  setColor('React Dev TypeScript'),
+        description: colors.error('React + Vite + TypeScript + Tailwind CSS + ESlint + Jest + React Testing Library.'),
+      },
+    ]
   },
-  {
-    value: 'minimal-react',
-    title: colors.react('Minimal React'),
-    description: colors.error('React + Vite.'),
-  },
-  {
-    value: 'minimal-react-wp',
-    title: colors.webpack('Minimal React Webpack'),
-    description: colors.error('React + Webpack.')
-  },
-  {
-    value: 'react-tw',
-    title:  colors.react('React Tailwind CSS'),
-    description: colors.error('React + Vite + Tailwindcss.'),
-  },
-  {
-    value: 'react-ts',
-    title:  colors.typescript('React TypeScript'),
-    description: colors.error('React + Vite + TypeScript + ESlint.'),
-  },
-  {
-    value: 'react-ex',
-    title:  colors.experimental('React Experimental'),
-    description: colors.error('React 19 + Farm + Oxlint.'),
-  },
-  {
-    value: 'react-dev',
-    title:  colors.react('React Dev'),
-    description: colors.error('React + Vite + Tailwind CSS + ESlint + Jest + React Testing Library.'),
-  },
-  {
-    value: 'react-dev-ts',
-    title:  colors.typescript('React Dev Typescript'),
-    description: colors.error('React + Vite + TypeScript + Tailwind CSS + ESlint + Jest + React Testing Library.'),
-  },
-  {
-    value: 'react-dev-rolldown',
-    title: colors.rust('React Dev Rolldown'),
-    description: colors.error('React + Vite (Rolldown) + Tailwind CSS + ESlint + Jest + React Testing Library.'),
-  },
-  {
-    value: 'react-dev-webpack',
-    title: colors.webpack('React Dev Webpack'),
-    description: colors.error('React + Webpack + Tailwind CSS + ESlint + Jest + React Testing Library.')
-  },
-  {
-    value: 'vue-js',
-    title:  colors.vue('Vue'),
-    description: colors.error('Vue + Vite'),
-  },
-  {
-    value: 'node-express',
-    title:  colors.node('Node Express'),
-    description: colors.error('Node + Express + Jest + Eslint.'),
+  'Backend': {
+    'NodeJS': [
+      {
+        value: 'node-express',
+        title:  setColor('Node Express'),
+        description: colors.error('Node + Express + Jest + Eslint.'),
+      }
+    ]
   }
-];
+}
 
-export default templates;
+export default templateCategories;
